11563338da6c0ec71ac96763295121b49663c26e
kotek <scpktk@gmail.com>
2025-02-01 14:57:15 +0900

    worked

--- M3508Test02.ino
+++ M3508Test02.ino
@@ -3,16 +3,22 @@
 
 #define CAN_ID (0x200)
 #define CAN_SEND_INTERVAL (100)
+#define CAN_RECEIVE_INTERVAL (1000)
 
 MCP_CAN CAN(10); // CAN CS: pin 10
+// CS: pin 10
+// MISO: pin 12
+// MOSI: pin 11
+// SCK: pin 13
 
-const int milli_amperes[4] = {10, 0, 0, 0};
+const signed long sendingMilliAmperes[4] = {500, 0, 0, 0};
 
-void scaleCurrentToRange(const signed int milliAmperes[4], byte outTxBuf[8]);
+void scaleCurrentToRange(const signed long milliAmperes[4], byte outTxBuf[8]);
 void deriveFeedbackFields(const byte rxBuf[8], float *outAngle, unsigned int *outRpm, signed int *outAmp, unsigned int *outTemp);
 
 /// @brief 前回のアップデート時間
-long previousMillis;
+long previousSendMillis;
+long previousReceiveMillis;
 
 void setup()
 {
@@ -30,7 +36,8 @@ void setup()
         Serial.println("Init Fail!");
     }
 
-    previousMillis = millis();
+    previousSendMillis = millis();
+    previousReceiveMillis = millis();
 }
 
 void loop()
@@ -40,66 +47,79 @@ void loop()
     static signed int amp;
     static unsigned int temp;
 
-    static int targetRpm;
+    static int targetRpm = 100;
 
-    static const float kp = 0.000001f;
+    static const float kp = 0.001f;
     static const float ki = 0.0f;
     static const float kd = 0.0f;
     static float integral;
     static float previousError;
 
-    if ((millis() - previousMillis) > CAN_SEND_INTERVAL)
+    if ((millis() - previousSendMillis) > CAN_SEND_INTERVAL)
     {
         signed int currentError = targetRpm - rpm;
         integral += currentError * CAN_SEND_INTERVAL;
-        float derivative = (currentError - previousError) / CAN_SEND_INTERVAL; 
+        float derivative = (currentError - previousError) / CAN_SEND_INTERVAL;
         float output = kp * currentError + ki * integral + kd * derivative;
         previousError = currentError;
-        output = min(output, 5000);  // 安全装置
+        output = min(output, 5000); // 安全装置
 
-        Serial.println("output: ");
-        Serial.println(output);
-        Serial.println("\n");
+        // Serial.print("output: ");
+        // Serial.print(output);
+        // Serial.print("\n");
 
         byte txBuf[8];
-        scaleCurrentToRange(milli_amperes, txBuf);
+        scaleCurrentToRange(sendingMilliAmperes, txBuf);
         CAN.sendMsgBuf(CAN_ID, 0, 8, txBuf);
-        Serial.println("Send ID: 0x200");
-        previousMillis = millis();
+        // Serial.print("Send ID: 0x200");
+        // Serial.print("\n");
+        // Serial.print("txBuf: ");
+        // for (byte i = 0; i < 8; i++)
+        // {
+        //     Serial.print(txBuf[i], HEX);
+        //     Serial.print(" ");
+        // }
+        // Seril.print("\n");
+        previousSendMillis = millis();
     }
 
-    if (CAN.checkReceive() == CAN_MSGAVAIL)
+    if ((millis() - previousReceiveMillis) > CAN_RECEIVE_INTERVAL)
     {
-        unsigned long rxId;
-        byte length;
-        byte rxBuf[8];
-        CAN.readMsgBuf(&rxId, &length, rxBuf);
-        Serial.print("Receive ID: ");
-        Serial.print(rxId, HEX);
-        Serial.print("Data: ");
-        for (byte i = 0; i < length; i++)
+        if (CAN.checkReceive() == CAN_MSGAVAIL)
         {
-            Serial.print(rxBuf[i], HEX);
-            Serial.print(" ");
+            unsigned long rxId;
+            byte length;
+            byte rxBuf[8];
+            CAN.readMsgBuf(&rxId, &length, rxBuf);
+            Serial.print("Receive ID: ");
+            Serial.print(rxId, HEX);
+            Serial.print("\n");
+            Serial.print("Data: ");
+            for (byte i = 0; i < length; i++)
+            {
+                Serial.print(rxBuf[i], HEX);
+                Serial.print(" ");
+            }
+            Serial.print("\n");
+
+            unsigned long controllerId = rxId - 0x200;
+
+            deriveFeedbackFields(rxBuf, &angle, &rpm, &amp, &temp);
+
+            Serial.print("angle: ");
+            Serial.print(angle);
+            Serial.print(", ");
+            Serial.print("rpm: ");
+            Serial.print(rpm);
+            Serial.print(", ");
+            Serial.print("amp: ");
+            Serial.print(amp);
+            Serial.print(", ");
+            Serial.print("temp: ");
+            Serial.print(temp);
+            Serial.print("\n");
         }
-        Serial.print("\n");
-
-        unsigned long controllerId = rxId - 0x200;
-
-        deriveFeedbackFields(rxBuf, &angle, &rpm, &amp, &temp);
-
-        Serial.print("angle: ");
-        Serial.print(angle);
-        Serial.print(", ");
-        Serial.print("rpm: ");
-        Serial.print(rpm);
-        Serial.print(", ");
-        Serial.print("amp: ");
-        Serial.print(amp);
-        Serial.print(", ");
-        Serial.print("temp: ");
-        Serial.print(temp);
-        Serial.print("\n");
+        previousReceiveMillis = millis();
     }
 
     digitalWrite(13, HIGH);
@@ -108,12 +128,12 @@ void loop()
 /// @brief 4つの電流値を、CANで速度コントローラに送信するデータへ変換
 /// @param milliAmperes 4つの-20000\~20000の電流値(mA)を格納した配列 (要素番号と速度コントローラIDが対応)
 /// @param outTxBuf 結果の書き込み用配列
-void scaleCurrentToRange(const signed int milliAmperes[4], byte outTxBuf[8])
+void scaleCurrentToRange(const signed long milliAmperes[4], byte outTxBuf[8])
 {
-    int i;
+    byte i;
     for (i = 0; i < 4; i++)
     {
-        int milliAmpere = milliAmperes[i] * 16384 / 20000;
+        long milliAmpere = milliAmperes[i] * 16384 / 20000;
         byte upper = (milliAmpere >> 8) & 0xFF;
         byte lower = milliAmpere & 0xFF;
         outTxBuf[i * 2] = upper;
