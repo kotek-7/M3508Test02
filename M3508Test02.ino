cbaa5a760ec3fe18ec7142009b07b59a05f95747
kotek <scpktk@gmail.com>
2025-02-01 10:24:55 +0900

    Add function to convert array

--- M3508Test02.ino
+++ M3508Test02.ino
@@ -1,10 +1,17 @@
 #include <mcp_can.h>
 #include <SPI.h>
 
-byte tx_buf[] = {0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-long previous_millis;
+#define CAN_ID (0x200)
+#define CAN_SEND_INTERVAL (100)
 
 MCP_CAN CAN(10); // CAN CS: pin 10
+
+const int milli_amperes[4] = {10, 0, 0, 0};
+
+void scaleCurrentToRange(signed int milliAmperes[4], byte outTxBuf[8]);
+
+/// @brief 前回のアップデート時間
+long previousMillis;
 
 void setup()
 {
@@ -22,35 +29,53 @@ void setup()
         Serial.println("Init Fail!");
     }
 
-    previous_millis = millis();
+    previousMillis = millis();
 }
 
 void loop()
 {
-    static int count = 0;
-    if (millis() - previous_millis > 100)
-    { // Period: 100ms
-        CAN.sendMsgBuf(0x200, 0, 8, tx_buf);
+    if ((millis() - previousMillis) > CAN_SEND_INTERVAL)
+    {
+        byte txBuf[8];
+        scaleCurrentToRange(milli_amperes, txBuf);
+        CAN.sendMsgBuf(CAN_ID, 0, 8, txBuf);
         Serial.println("Send ID: 0x200");
-        previous_millis = millis();
+        previousMillis = millis();
     }
 
     if (CAN.checkReceive() == CAN_MSGAVAIL)
     {
-        unsigned long rx_id;
+        unsigned long rxId;
         byte length;
-        byte rx_buf[8];
-        CAN.readMsgBuf(&rx_id, &length, rx_buf);
-        Serial.print("Recive ID: ");
-        Serial.print(rx_id, HEX);
+        byte rxBuf[8];
+        CAN.readMsgBuf(&rxId, &length, rxBuf);
+        Serial.print("Receive ID: ");
+        Serial.print(rxId, HEX);
         Serial.print("Data: ");
         for (byte i = 0; i < length; i++)
         {
-            Serial.print(rx_buf[i], HEX);
+            Serial.print(rxBuf[i], HEX);
             Serial.print(" ");
         }
         Serial.println();
+
     }
 
     digitalWrite(13, HIGH);
+}
+
+/// -20000mA\~20000mAの4要素の配列を、CANで速度コントローラに送信する8バイトの配列に変換
+/// @param milliAmperes -20000\~20000の電流値(mA)を持つの長さ4の配列. 要素番号と速度コントローラのIDが対応してる
+/// @param outTxBuf 結果の書き込み用配列
+void scaleCurrentToRange(signed int milliAmperes[4], byte outTxBuf[8])
+{
+    int i;
+    for (i = 0; i < 4; i++)
+    {
+        int milliAmpere = milliAmperes[i] * 16384 / 20000;
+        byte upper = (milliAmpere >> 8) & 0xFF;
+        byte lower = milliAmpere & 0xFF;
+        outTxBuf[i * 2] = upper;
+        outTxBuf[i * 2 + 1] = lower;
+    }
 }
